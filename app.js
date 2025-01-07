57e2e9e5ca452d29d6bf808fda11df1eac714ff7
Kage Saluone <ksaluone17@gmail.com>
2025-01-07 09:39:24 -0700

    Created Friends Form and some function to it

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import express from 'express';
-// const express = require('express');
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+const friends = [];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -9,8 +10,40 @@ app.use(express.static('public'));
 
 app.get('/getCurrentDate', (req, res) => {
     const date = new Date()
-    res.json({'currentDate': date});
+    res.json({ 'currentDate': date });
 })
+
+app.post('/addFriend', (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const phone = req.body.phone
+    const age = req.body.age
+    const friend = {
+        name: name,
+        email: email,
+        phone: phone,
+        age: age
+    }
+    friends.push(friend);
+    res.json({ 'friends': friends })
+})
+
+app.post('/findFriend', (req, res) => {
+    console.log('Request body:', req.body);
+    console.log('Friends List:', friends);
+
+    const { findFriend } = req.body;
+    const foundFriend = friends.find((f) => f.email === findFriend)
+
+    if (foundFriend) {
+        res.send(`User Found: ${foundFriend.name}`);
+    } else {
+        res.send(`User not found`);
+    }
+})
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
